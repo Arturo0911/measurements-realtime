e5fea85eb58a61242c1f11623b9b572d88648d16
Arturo0911 <anegreiross@outlook.com>
2021-11-30 18:33:26 -0500

    Missing import LoadEnv() xD

--- connection/env.go
+++ connection/env.go
@@ -6,7 +6,7 @@ import (
 	"github.com/joho/godotenv"
 )
 
-func loadEnv() {
+func LoadEnv() {
 	err := godotenv.Load(".env")
 	if err != nil {
 		log.Fatalf("unable to fetch the environments variables %s", err)
--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	. "github.com/Arturo0911/measurements-realtime/connection"
 	. "github.com/Arturo0911/measurements-realtime/server"
 )
 
 func main() {
+	LoadEnv()
 	Serve()
 	//fmt.Println("")
 	//DataBaseConnection()
